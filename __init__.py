21e2168bc81ba3ddfb5ae3f19b01f49aa3913aa8
Eduardo Lemus Geo <86122642+eduardolemus-geo@users.noreply.github.com>
2025-08-22 13:02:09 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,19 +3,38 @@
 # Copyright (C) 2025 Eduardo Lemus
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-# Este archivo es parte de CartoMinMex_QGIS.
-# Se distribuye bajo los términos de la GNU General Public License,
-# versión 3 o posterior. ESTE PROGRAMA SE ENTREGA "TAL CUAL", SIN
-# GARANTÍA; consulta el archivo LICENSE para más detalles.
-# Copia de la licencia: archivo LICENSE incluido en el paquete
-# y https://www.gnu.org/licenses/gpl-3.0.en.html
+# This file is part of CartoMinMex_QGIS.
+# It is distributed under the terms of the GNU General Public License,
+# version 3 or later. THIS PROGRAM IS PROVIDED "AS IS", WITHOUT
+# WARRANTY; see the LICENSE file for more details.
 
-__author__ = "Eduardo Lemus"
-__email__ = "eduardo.lemusm@fi.unam.edu"
-__license__ = "GPL-3.0-or-later"
-__version__ = "1.0.0"
+"""
+Plugin bootstrap module.
 
+Responsibilities:
+- Expose `classFactory(iface)` as the QGIS entry point.
+- Defer all heavy logic to the main plugin class to keep startup fast.
+- Keep imports light and side effects minimal.
+"""
+
+# Import the main plugin class from the local package.
+# Using a relative import avoids issues when the plugin is moved/packaged.
 from .cartominmex_qgis import CartoMinMexPlugin
 
+
 def classFactory(iface):
+    """
+    QGIS calls this to instantiate the plugin.
+
+    Parameters
+    ----------
+    iface : qgis.gui.QgsInterface
+        The QGIS interface object provided by the host application.
+
+    Returns
+    -------
+    CartoMinMexPlugin
+        A fully constructed plugin instance. Heavy initialization should be
+        done inside the plugin's `initGui()` rather than here.
+    """
     return CartoMinMexPlugin(iface)
